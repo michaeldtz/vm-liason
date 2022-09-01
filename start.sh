041b247126c75ad6ab79dee3c34fcd1826a7a182
Michael Dietz <michael.dtz@gmail.com>
2022-09-01 13:24:37 +0200

    Cleanup

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 gcloud compute instances create vm-1 \
 --zone=europe-west3-a --machine-type=n2d-standard-4 \
 --network-interface=subnet=default,no-address \
---metadata-from-file=startup-script=script_preparation.sh \
+--metadata-from-file=startup-script=vm1_startup_script.sh \
 --service-account=1035704952186-compute@developer.gserviceaccount.com \
 --scopes=https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_only \
 --create-disk=auto-delete=yes,boot=yes,device-name=sda,image=projects/opensuse-cloud/global/images/opensuse-leap-15-4-v20220722-x86-64,mode=rw,size=10,type=projects/midietz-one/zones/europe-west3-a/diskTypes/pd-balanced \
